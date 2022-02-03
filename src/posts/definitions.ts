01d1976e4e314966da0dd1cf4b9dd9897c28c09c
Johann Munoz <johannmunoz.dev@gmail.com>
2022-02-04 10:25:07 +1030

    Added posts crud

--- src/posts/definitions.ts
+++ src/posts/definitions.ts
@@ -2,24 +2,48 @@ import { gql } from 'apollo-server'
 
 export const postTypeDefs = gql`
   type Post {
-    content: String
-    createdAt: DateTime!
-    id: Int!
-    published: Boolean!
+    id: ID!
     title: String!
-    updatedAt: DateTime!
+    content: String
+    published: Boolean!
     viewCount: Int!
+    createdAt: DateTime
+    updatedAt: DateTime
   }
 
   extend type User {
     posts: [Post!]!
   }
+
   extend type Account {
     posts: [Post!]!
+  }
+
+  type PostPayload {
+    success: Boolean!
+    post: Post
   }
 
   extend type Query {
     posts: [Post!]!
     post(id: String!): Post!
+  }
+
+  extend type Mutation {
+    postCreate(input: PostCreateInput!): PostPayload!
+    postUpdate(id: String!, input: PostUpdateInput!): PostPayload!
+    postDelete(id: String!): ArchivePayload!
+  }
+
+  input PostCreateInput {
+    authorId: String!
+    accountId: String!
+    title: String!
+    content: String!
+  }
+
+  input PostUpdateInput {
+    title: String!
+    content: String!
   }
 `
--- src/posts/resolvers.ts
+++ src/posts/resolvers.ts
@@ -4,6 +4,11 @@ import { GqlApi } from '../gql'
 import { postTypeDefs } from './definitions'
 
 const resolvers = {
+  Mutation: {
+    postCreate,
+    postUpdate,
+    postDelete,
+  },
   Query: {
     posts,
     post,
@@ -25,7 +30,7 @@ async function posts(root: any, _: any, context: Context) {
   }
 }
 
-async function post(root: any, id: number, context: Context) {
+async function post(root: any, { id }, context: Context) {
   try {
     const data = await context.prisma.post.findUnique({ where: { id: id } })
     return data
@@ -53,6 +58,45 @@ async function userPosts(user: User, _: any, context: Context) {
     return data
   } catch (error) {
     console.error(error)
+  }
+}
+
+async function postCreate(root: any, { input }, context: Context) {
+  try {
+    const newPost = input
+
+    const data = await context.prisma.post.create({ data: newPost })
+    return { success: true, post: data }
+  } catch (error) {
+    console.error(error)
+    return { success: false, post: null }
+  }
+}
+
+async function postUpdate(root: any, { id, input }, context: Context) {
+  try {
+    const postData = input
+
+    const data = await context.prisma.post.update({
+      where: { id: id },
+      data: postData,
+    })
+    return { success: true, post: data }
+  } catch (error) {
+    console.error(error)
+    return { success: false, post: null }
+  }
+}
+
+async function postDelete(root: any, { id }, context: Context) {
+  try {
+    await context.prisma.post.delete({
+      where: { id: id },
+    })
+    return { success: true }
+  } catch (error) {
+    console.error(error)
+    return { success: false }
   }
 }
 
